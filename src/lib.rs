1d6d64440de87ef1734c9428e640755a692c8142
Weird Constructor <weirdconstructor@gmail.com>
2019-05-17 21:28:13 +0200

    Wrote some reasoning about the GPL choice.

--- src/lib.rs
+++ src/lib.rs
@@ -152,7 +152,51 @@ evaluation this can be changed.
 This project is licensed under the GNU General Public License Version 3 or
 later.
 
-If you need this crate in a more Rust compatible Licesen, please contact me.
+## Why GPL?
+
+I've long thought about this, which license should I use for my published
+code. And I've read and had some discussions about this. First
+about _why I write code for free_ at all:
+
+- It's my passion to write computer programs. And in my free time I can
+write the code I want, when I want and the way I want. I can freely
+allocate my time and freely choose the projects I want to work on.
+- To help a friend or member of my family.
+- To solve a problem I have.
+
+Those are the reasons why I write code for free. Now the reasons
+_why I publish the code_, when I could as well keep it to myself:
+
+- So that it may bring value to users and the free software community.
+- Show my work as an artist.
+- To get into contact with other developers.
+- Have a reason to put more of the last 20% of work into a project
+to make it more complete.
+
+Most of those reasons don't yet justify GPL. The main point of the GPL, as far
+as I understand: The GPL makes sure the software stays free software until
+eternity. That the user of the software always stays in control. That the users
+have _at least the means_ to adapt the software to new platforms or use cases.
+Even if the original authors don't maintain the software anymore.
+It ultimately prevents _"vendor lock in"_. I really dislike vendor lock in,
+especially as developer. Especially as developer I want and need to stay
+in control of the computers I use.
+
+Another point is, that I have to pay my bills with my development skill.
+If I give away my work without _any_ strings attached, I effectively work
+for free. Work for free for companies. I would compromise the price I can
+demand for my skill, workforce and time.
+
+This makes two reasons for me to choose the GPL:
+
+1. I do not want to support vendor lock in scenarios. At least not for free.
+   I want to prevent those when I have a choice.
+   And before you ask, yes I work for a company that sells closed source
+   software. I am not happy about the closed source fact.
+   But it pays my bills and gives me the freedom to write free software at least
+   in my free time.
+2. I don't want to low ball my own wage and prices by giving away free software
+   with no strings attached (for companies).
 
 # Contribution
 
