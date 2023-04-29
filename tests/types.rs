431806f537c2af9c46f30e3a779d906db1ae4182
Weird Constructor <weirdconstructor@gmail.com>
2023-04-29 09:07:26 +0200

    forgot the tests

---
+++ tests/types.rs
@@ -0,0 +1,39 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+use wlambda::*;
+
+pub fn v(s: &str) -> String {
+    let mut ctx = EvalContext::new_default();
+    match ctx.eval(s) {
+        Ok(v) => v.s(),
+        Err(e) => format!("{}", e),
+    }
+}
+
+pub fn v2s(s: &str) -> String {
+    let mut ctx = EvalContext::new_default();
+    match ctx.eval(s) {
+        Ok(v) => v.with_s_ref(|s| String::from(s)),
+        Err(e) => format!("{}", e),
+    }
+}
+
+fn ve(s: &str) -> String {
+    wlambda::compiler::test_eval_to_string(s)
+}
+
+#[test]
+fn check_type_simple() {
+    assert_eq!(ve("!v @Int = 10; v"), "10");
+    assert_eq!(ve("!v @Int = 10.1; v"),
+        "COMPILE ERROR: <compiler:s_eval>:1:2 Compilation Error: Incompatible types: Int <=> Float");
+    assert_eq!(ve("!v @Dyn = 10.1; v"), "10.1");
+    assert_eq!(
+        ve("!(a @Float, b @Float) = $p(10, 11.1); a"),
+        "COMPILE ERROR: Float != Int"
+    );
+    assert_eq!(
+        ve("!(a @Int, b @Float) = $p(10, 11.1); a + int[b]"),
+        "21"
+    );
+}
